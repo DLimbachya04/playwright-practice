6b5ef8a9451c8b82485b2cb08b200520f36bdf9e
Devan Limbachya <Devan_limbachya98@hotmail.co.uk>
2026-07-11 16:35:44 +0100

    Add invalid login test with error assertion

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,10 +3,24 @@ import { test, expect } from '@playwright/test';
 test('successful login', async ({ page }) => {
   await page.goto('https://www.saucedemo.com');
   
-  // your code here
   await expect(page).toHaveTitle(/Swag Labs/);
 
   await page.getByPlaceholder('Username').fill('standard_user'); 
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByRole('button', {name: 'Login' }).click();
+  await expect(page).toHaveURL(/inventory.html/);
+
 });
+
+test('invalid login shows error', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  
+  await page.getByPlaceholder('Username').fill('standard_user');
+  await page.getByPlaceholder('Password').fill('wrong_password');
+  await page.getByRole('button', { name: 'Login' }).click();
+ 
+  await expect(page.getByText('Epic sadface: Username and password do not match any user in this service')).toBeVisible();
+});
+
 
 
